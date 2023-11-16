74e4e2e76f13e6d364707d19f5746e83b88091ce
washalbano <washalbano@gmail.com>
2023-11-16 02:38:41 -0300

    feat: add cursor to dropdown menu items

--- src/main.js
+++ src/main.js
@@ -7,5 +7,14 @@ const toggleSidebar = (className, e) => {
   Body.classList.toggle(className)
 }
 
+const toggleSidebarItem = (e) => {
+  e.preventDefault()
+  sidebarLinks.forEach((el) => el !== e.target && el.classList.remove('active'))
+  e.target.classList.toggle('active')
+}
+
 document.querySelector('a.hamburger').onclick = (e) => toggleSidebar('sidebar-show', e)
 document.querySelector('a.sidebar-mini').onclick = (e) => toggleSidebar('sidebar-mini', e)
+
+const sidebarLinks = document.querySelectorAll('aside nav a')
+sidebarLinks.forEach((el) => el.addEventListener('click', toggleSidebarItem))
