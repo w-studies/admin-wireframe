f72f195b18ad40e67dba141871c5026dc8ff779c
washalbano <washalbano@gmail.com>
2023-11-15 22:05:48 -0300

    refactor: create function to toggleSidebar actions

--- src/main.js
+++ src/main.js
@@ -2,14 +2,10 @@ import './scss/style.scss'
 
 const Body = document.querySelector('body')
 
-document.querySelector('a.hamburger').onclick = (e) => {
+toggleSidebar = (className, e) => {
   e.preventDefault()
-
-  Body.classList.toggle('sidebar-show')
+  Body.classList.toggle(className)
 }
 
-document.querySelector('a.sidebar-mini').onclick = (e) => {
-  e.preventDefault()
-
-  Body.classList.toggle('sidebar-mini')
-}
+document.querySelector('a.hamburger').onclick = (e) => toggleSidebar('sidebar-show', e)
+document.querySelector('a.sidebar-mini').onclick = (e) => toggleSidebar('sidebar-mini', e)
